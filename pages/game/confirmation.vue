518f6ece541f50f2c10456384b5d1e4b06032f77
Terence Bezman <terencebezman@gmail.com>
2018-02-27 20:22:48 -0500

    Game confirmation

---
+++ pages/game/confirmation.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="ribbon"></div>
+        <div class="container">
+            <div class="row-lift">
+                <div class="heading">
+                    <h1>You've been entered!</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-offset">
+            <div class="container">
+                <div class="card">
+                    <div class="card__inner">
+                        <div class="form-group">
+                            <h2>What's next?</h2>
+                            <p>You will receive an email confirmation if you have been selected to play. Only 6 players
+                                will be selected, one for each position.</p>
+                        </div>
+
+                        <div class="form-group">
+                            <h2>How to prepare</h2>
+                            <p class="form-group">
+                                There are a lot of things you should probably catch up on before game day. Below is our
+                                DevWars Documentation covering everything you need to know.
+                            </p>
+                            <a href="/docs" class="btn btn-primary">View Documentation</a>
+                        </div>
+
+                        <p>Have any questions? <a href="/contact">Contact Us</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Component from 'nuxt-class-component';
+    import Vue from 'vue';
+
+    @Component
+    export default class GameConfirmation extends Vue {
+    }
+</script>
