42cd5e968c46e415e96c2176afa55484f8a2ef50
Terence Bezman <terencebezman@gmail.com>
2018-03-01 06:43:22 -0500

    Brief applicant listing

--- pages/mod/game/brief.vue
+++ pages/mod/game/brief.vue
@@ -12,7 +12,8 @@
                         </div>
                     </div>
                     <div class="games-players">
-                        <div class="games-player" :key="player.id" v-for="player in game.teams.blue.players" ng-click="mod.removePlayer(player)">
+                        <div class="games-player" :key="player.id" v-for="player in game.teams.blue.players"
+                             ng-click="mod.removePlayer(player)">
                             <div class="user-group">
                                 <div class="games-player__avatar">
                                     <Avatar :user="player.user"/>
@@ -74,47 +75,48 @@
 
         <br><br>
 
-        <!--<div class="modpanel-table">-->
-            <!--<br>-->
-            <!--<h3 class="modpanel__subtitle">({{ applications.length }}) Applicants</h3>-->
-            <!--<table>-->
-                <!--<thead>-->
-                <!--<tr>-->
-                    <!--<th>Username</th>-->
-                    <!--<th>Rank</th>-->
-                    <!--<th>Games</th>-->
-                    <!--<th>Won</th>-->
-                    <!--<th>Lost</th>-->
-                    <!--<th>HTML</th>-->
-                    <!--<th>CSS</th>-->
-                    <!--<th>JS</th>-->
-                    <!--<th>&nbsp;</th>-->
-                <!--</tr>-->
-                <!--</thead>-->
-                <!--<tbody>-->
-                <!--<tr v-for="user in applications" :key="user.id">-->
-                    <!--<td>-->
-                        <!--<img class="modpanel-table__item_image" ng-src="{{ user.avatarURL }}">-->
-                        <!--<span class="modpanel-table__item_name">{{ user.username }}</span>-->
-                    <!--</td>-->
-                    <!--<td>{{ user.rank.rank }}</td>-->
-                    <!--<td>{{ user.gamesPlayed }}</td>-->
-                    <!--<td>{{ user.gamesWon}}</td>-->
-                    <!--<td>{{ user.gamesLost}}</td>-->
-                    <!--<td>{{ user.warrior.htmlRate}}</td>-->
-                    <!--<td>{{ user.warrior.cssRate }}</td>-->
-                    <!--<td>{{ user.warrior.jsRate }}</td>-->
-                    <!--<td class="color-devbits">{{ user.devBits | number : fractionSize }}</td>-->
-                    <!--<td class="modpanel-table__actions">-->
-                        <!--<a href="#edit" class="btn-link btn-icon-reverse" ng-click="mod.addPlayer(user)">-->
-                            <!--<span>Add Player</span>-->
-                            <!--<i class="fa fa-caret-down"></i>-->
-                        <!--</a>-->
-                    <!--</td>-->
-                <!--</tr>-->
-                <!--</tbody>-->
-            <!--</table>-->
-        <!--</div>-->
+        <div class="modpanel-table">
+            <br>
+            <h3 class="modpanel__subtitle">({{ applications.length }}) Applicants</h3>
+            <table>
+                <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Rank</th>
+                    <th>Games</th>
+                    <th>Won</th>
+                    <th>Lost</th>
+                    <th>HTML</th>
+                    <th>CSS</th>
+                    <th>JS</th>
+                    <th>Bits</th>
+                    <th>&nbsp;</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="application in applications" :key="application.user.id">
+                    <td>
+                        <img class="modpanel-table__item_image" :src="application.user.avatar_url">
+                        <span class="modpanel-table__item_name">{{ application.user.username }}</span>
+                    </td>
+                    <td>{{ application.user.ranking.rank.rank }}</td>
+                    <td>{{ application.wins + application.losses }}</td>
+                    <td>{{ application.wins }}</td>
+                    <td>{{ application.losses }}</td>
+                    <td>{{ rating(application.user, 'html')}}</td>
+                    <td>{{ rating(application.user, 'css')}}</td>
+                    <td>{{ rating(application.user, 'js')}}</td>
+                    <td class="color-devbits">{{ application.user.ranking.bits | number }}</td>
+                    <td class="modpanel-table__actions">
+                        <a href="#edit" class="btn-link btn-icon-reverse" ng-click="mod.addPlayer(user)">
+                            <span>Add Player</span>
+                            <i class="fa fa-caret-down"></i>
+                        </a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -122,11 +124,28 @@
     import Component, {State} from 'nuxt-class-component';
     import Vue from 'vue';
 
+    import Avatar from '~/components/user/Avatar';
+    import Http from "../../../services/Http";
+
     @Component({
-        components: {}
+        components: {Avatar}
     })
     export default class  extends Vue {
         @State(state => state.game.game) game;
+        applications = [];
 
+        rating(user, lang) {
+            if(!user.competitor) return 0;
+
+            return user.competitor[lang + '_rate'];
+        }
+
+        async mounted() {
+            let applications = await Http.for('game/application').get(`${this.game.id}`);
+
+            this.applications = applications;
+
+            console.log(this.applications);
+        }
     }
 </script>
