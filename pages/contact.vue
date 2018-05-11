887718c70da3f80b14fce1aa19da4d146d3f351d
SYNTAG <accounts@blucitrus.com>
2018-05-11 15:57:41 -0700

    Added Contact Page

---
+++ pages/contact.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="ribbon"></div>
+
+        <div class="footer-offset container">
+            <div class="row-lift">
+                <div class="heading">
+                    <h1>Contact</h1>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="card card-plain">
+                            <div class="card__inner">
+                                <form async-submit="contact.contact()">
+                                    <div class="form-group">
+                                        <input ng-model="contact.info.name" class="form-control" type="text" required>
+                                        <label>Name</label>
+                                    </div>
+                                    <div class="form-group">
+                                        <input ng-model="contact.info.email" class="form-control" type="text" required>
+                                        <label>Email</label>
+                                    </div>
+                                    <div class="form-group">
+                                        <textarea ng-model="contact.info.text" class="form-control" rows="8" placeholder="Message"></textarea>
+                                    </div>
+
+                                    <div class="align-right">
+                                        <button type="submit" class="btn btn-primary">Send Email</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card card-plain">
+                            <div class="card__inner">
+                                <div class="contact-sidebar">
+                                    <div class="contact-sidebar__address">
+                                        contact@devwars.tv
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Component from 'nuxt-class-component';
+    import Vue from 'vue';
+
+    @Component({})
+
+    export default class Contact extends Vue {
+
+    }
+</script>
