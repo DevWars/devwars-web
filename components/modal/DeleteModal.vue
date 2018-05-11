d962e4cbed47145abb2ac80b044888d40bd3c6dc
SYNTAG <accounts@blucitrus.com>
2018-05-11 16:53:56 -0700

    Added Delete Modal Template

---
+++ components/modal/DeleteModal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="modal">
+        <h1 class="modal__title">{{ title }}</h1>
+        <p class="modal__desc" v-if="description">{{ description }}</p>
+
+        <div class="modal-actions">
+            <button class="btn btn-link" @click="$emit('close')"> {{ cancel }}</button>
+            <button class="btn btn-danger" @click="close(true)">{{ confirm }}</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Component from 'nuxt-class-component';
+    import Vue from 'vue';
+
+    import {Prop} from 'vue-property-decorator';
+
+    @Component({})
+    export default class extends Vue {
+        @Prop({default: "Warning!"}) title;
+        @Prop() description;
+        @Prop({default: "Cancel"}) cancel;
+        @Prop({default: "Delete"}) confirm;
+
+        @Prop() resolve;
+        @Prop() reject;
+    }
+</script>
