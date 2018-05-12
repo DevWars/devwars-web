f9fdfb322d6ef51c68b13e61d7f324afd28afcd9
SYNTAG <accounts@blucitrus.com>
2018-05-12 01:46:46 -0700

    Added already registered state for competitors

--- pages/competitor/register.vue
+++ pages/competitor/register.vue
@@ -134,6 +134,20 @@
                         </div>
                     </form>
                 </div>
+
+
+                <div class="mod-card">
+                    <h3 class="modpanel__title">You are already registered as a Competitor</h3>
+                    <p>Want to compete in an upcoming game?</p>
+                    <div class="mt-30">
+                        <a href="/schedule" class="btn btn-primary mr-15">View schedule</a>
+                        <a href="/" class="btn btn-outline-primary">Back to home</a>
+                    </div>
+                </div>
+
+
+
+
             </div>
         </div>
     </div>
