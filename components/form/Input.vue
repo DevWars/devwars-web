0734b2db2d58ef1994070b5ef7c63cd0a89b864d
Terence Bezman <terencebezman@gmail.com>
2018-06-30 17:42:56 -0400

    Input component with empty class when empty

---
+++ components/form/Input.vue
@@ -0,0 +1,22 @@
+<template>
+    <input
+        :type="type"
+        :value="value"
+        @input="e => $emit('input', e.target.value)"
+        :class="['form-control', {empty: !(value && value.length)}]"
+    />
+</template>
+
+<script>
+    import Component from 'nuxt-class-component';
+    import Vue from 'vue';
+
+    import { Prop } from 'vue-property-decorator';
+
+    @Component({})
+    export default class extends Vue {
+        @Prop() type;
+        @Prop() value;
+    }
+</script>
+
