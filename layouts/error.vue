7361f7e691aa25af3ef1a8acaeed4f6a7301ef5b
SYNTAG <accounts@blucitrus.com>
2018-11-13 15:25:51 -0700

    Scoped error styles

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="error-pg">
+    <div class="page">
         <div class="container">
             <div class="row">
                 <div v-if="error.statusCode === 404">
                     <div class="col-sm-4">
-                        <img src="~/assets/img/error-sad.png" alt="Error" class="error-pg__img">
+                        <img src="~/assets/img/error-sad.png" alt="Error">
                     </div>
                     <div class="col-sm-8">
-                        <h1 class="error-pg__code">404</h1>
-                        <h2 class="error-pg__title">Page not found</h2>
-                        <p class="error-pg__desc">The page you are looking for does not exist. Sorry!</p>
+                        <h1>404</h1>
+                        <h2>Page not found</h2>
+                        <p>The page you are looking for does not exist. Sorry!</p>
                         <nuxt-link to="/" class="btn btn-outline-primary btn-lg">Go to home</nuxt-link>
                     </div>
                 </div>
 
                 <div v-else-if="error.statusCode === 500">
                     <div class="col-sm-4">
-                        <img src="~/assets/img/error-sad.png" alt="Error" class="error-pg__img">
+                        <img src="~/assets/img/error-sad.png" alt="Error">
                     </div>
                     <div class="col-sm-8">
-                        <h1 class="error-pg__code">500</h1>
-                        <h2 class="error-pg__title">It's not you, It's us.</h2>
-                        <p class="error-pg__desc">This page is experiencing internal server issues <span
+                        <h1>500</h1>
+                        <h2>It's not you, It's us.</h2>
+                        <p>This page is experiencing internal server issues <span
                             class="font-size-sm"
                         >(blame Terence)</span></p>
                         <nuxt-link to="/" class="btn btn-outline-primary btn-lg">Go to home</nuxt-link>
@@ -30,11 +30,11 @@
 
                 <div v-else>
                     <div class="col-sm-4">
-                        <img src="~/assets/img/error-sad.png" alt="Error" class="error-pg__img">
+                        <img src="~/assets/img/error-sad.png" alt="Error">
                     </div>
                     <div class="col-sm-8">
-                        <h2 class="error-pg__title">Oops!</h2>
-                        <p class="error-pg__desc">Looks like something went wrong.</p>
+                        <h2>Oops!</h2>
+                        <p>Looks like something went wrong.</p>
                         <nuxt-link to="/" class="btn btn-outline-primary btn-lg">Go to home</nuxt-link>
                     </div>
                 </div>
@@ -53,3 +53,30 @@
         }
     }
 </script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/utils';
+
+.page {
+    padding: $l-space 0 $xl-space;
+}
+
+h1 {
+    font-size: $display1-size;
+}
+
+h2 {
+    margin-bottom: $xxs-space;
+}
+
+p {
+    margin-bottom: $ms-space;
+    font-size: $p-lead-font-size;
+    color: $text-color-secondary;
+}
+
+img {
+    max-width: 250px;
+    margin: 0 auto;
+}
+</style>
