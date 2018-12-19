c6feb36276b8da6f2d15a2294e5cf7dce988bb8b
SYNTAG <accounts@blucitrus.com>
2018-12-18 19:20:01 -0700

    Created ModSidebar Component

--- pages/mod.vue
+++ pages/mod.vue
@@ -1,71 +1,8 @@
 <template>
     <div class="ModPanel">
-        <div class="modpanel-sidebar">
-            <h1 class="modpanel-sidebar-header">Main</h1>
-            <ul class="modpanel-sidebar-list">
-                <li>
-                    <a href="/mod/dashboard" class="modpanel-sidebar-list-item">
-                    <i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a>
-                </li>
-                <li>
-                    <a href="/mod/users" class="modpanel-sidebar-list-item disabled">
-                    <i class="fa fa-user" aria-hidden="true"></i>Users</a>
-                </li>
-                <li>
-                    <a href="/mod/teams" class="modpanel-sidebar-list-item disabled">
-                    <i class="fa fa-users" aria-hidden="true"></i>Teams</a>
-                </li>
-                <li>
-                    <a href="/mod/games" class="modpanel-sidebar-list-item">
-                    <i class="fa fa-gamepad" aria-hidden="true"></i>Games</a>
-                </li>
-                <li>
-                    <a href="/mod/tournaments" class="modpanel-sidebar-list-item">
-                    <i class="fa fa-trophy" aria-hidden="true"></i>Tournaments</a>
-                </li>
-                <li>
-                    <a href="/mod/blogs" class="modpanel-sidebar-list-item disabled">
-                    <i class="fa fa-newspaper" aria-hidden="true"></i>Blog</a>
-                </li>
-            </ul>
+        <ModSidebar />
 
-            <h1 class="modpanel-sidebar-header">More</h1>
-            <ul class="modpanel-sidebar-list">
-                <li>
-                    <a href="#" class="modpanel-sidebar-list-item disabled">
-                    <i class="fa fa-file-alt" aria-hidden="true"></i>Mod Doc</a>
-                </li>
-            </ul>
-
-            <h1 class="modpanel-sidebar-header">Stats</h1>
-            <ul class="modpanel-sidebar-list modpanel-sidebar-list-info">
-                <li>
-                    <a href="#" class="modpanel-sidebar-list-item">
-                        <Progress
-                            title="Server Usage"
-                            :progress="30 + '%'"
-                            color="primary"
-                        />
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="modpanel-sidebar-list-item">
-                        <Progress
-                            title="GitHub Issues"
-                            :progress="30 + '%'"
-                            color="yellow"
-                        />
-                    </a>
-                </li>
-            </ul>
-
-            <button class="btn modpanel-sidebar-close">
-                <i class="icon icon-chevron"></i>
-            </button>
-        </div>
-
-
-        <div class="modpanel-main">
+        <div class="main">
             <nuxt/>
         </div>
     </div>
@@ -75,13 +12,12 @@
 import Component from 'nuxt-class-component';
 import Vue from 'vue';
 
-import Progress from '~/components/form/Progress';
+import ModSidebar from '~/components/mod/ModSidebar';
 
 @Component({
     layout: 'header',
-    components: { Progress }
+    components: { ModSidebar }
 })
-
 export default class Mod extends Vue {
 
 }
@@ -97,6 +33,13 @@ export default class Mod extends Vue {
 
     @include breakpoint(md) {
         display: block;
+    }
+
+    .main {
+        width: 100%;
+        background: $bg-color-3;
+        overflow-y: auto;
+        position: relative;
     }
 }
 </style>
