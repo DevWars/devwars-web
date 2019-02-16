158b29ae842ed8db63da28fc49eb1e15b8a87276
SYNTAG <accounts@blucitrus.com>
2019-02-16 13:25:31 -0700

    Styled ScheduleBlock

--- components/game/ScheduleBlock.vue
+++ components/game/ScheduleBlock.vue
@@ -1,46 +1,42 @@
 <template>
-    <div class="card card-plain">
-        <Applications :games="games">
-            <table class="table" slot-scope="{enterOrCancel, isApplied, cancel, enter}">
-                <thead>
-                <tr>
-                    <th width="15%" class="align-left">Date</th>
-                    <th width="15%" class="align-left">Time</th>
-                    <th width="5%" class="align-left">Duration</th>
-                    <th width="50%" class="align-left">Showing</th>
-                    <th width="20%" class="align-left"></th>
-                </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="game in limitBy(games, count)" :key="game.id" v-if="filter ? (filter === game.name) : true">
-                        <td>
-                            <div class="dow">{{ game.startTime | moment('dddd') }}</div>
-                            <h4 class="date">{{ game.startTime | moment('MMMM D') }}</h4>
-                        </td>
-                        <td>
-                            <h4 class="time">{{ game.startTime | moment('H:mm') }} UTC</h4>
-                        </td>
-                        <td>
-                            <h4 class="duration">{{ durations[game.name] || '30' }}</h4>
-                        </td>
-                        <td>
-                            <div class="show">DevWars Live</div>
-                            <div class="title">{{ description(game)}}</div>
-                        </td>
-                        <td>
-                            <a v-show="!isApplied(game)" @click="enter(game)" class="btn btn-primary">
-                                Register for Entry
-                            </a>
-                            <a
-                                v-show="isApplied(game)" @click="cancel(game)"
-                                class="btn btn-outline-danger"
-                            >Resign</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </Applications>
-    </div>
+    <Applications :games="games">
+        <Table slot-scope="{enterOrCancel, isApplied, cancel, enter}">
+            <tr slot="head">
+                <th width="15%" class="align-left">Date</th>
+                <th width="15%" class="align-left">Time</th>
+                <th width="5%" class="align-left">Duration</th>
+                <th width="40%" class="align-left">Showing</th>
+                <th width="30%" class="align-left"></th>
+            </tr>
+            <tr v-for="game in limitBy(games, count)" :key="game.id" v-if="filter ? (filter === game.name) : true">
+                <td>
+                    <div class="dow">{{ game.startTime | moment('dddd') }}</div>
+                    <h4 class="date">{{ game.startTime | moment('MMMM D') }}</h4>
+                </td>
+                <td>
+                    <h4 class="time">{{ game.startTime | moment('H:mm') }} UTC</h4>
+                </td>
+                <td>
+                    <h4 class="duration">{{ durations[game.name] || '30' }}</h4>
+                </td>
+                <td>
+                    <div class="show">DevWars Live</div>
+                    <div class="title">{{ description(game)}}</div>
+                </td>
+                <td>
+                    <div>
+                        <a v-show="!isApplied(game)" @click="enter(game)" class="btn btn-primary">
+                            Register for Entry
+                        </a>
+                        <a v-show="isApplied(game)" @click="cancel(game)" class="btn btn-outline-danger"
+                        >
+                            Resign
+                        </a>
+                    </div>
+                </td>
+            </tr>
+        </Table>
+    </Applications>
 </template>
 
 <script>
@@ -51,11 +47,12 @@
 
     import { Prop } from 'vue-property-decorator';
 
+    import Table from '~/components/Table';
     import Applications from '~/components/game/Applications';
     import GameDurations from '../../utils/game-durations';
 
     @Component({
-        components: { Applications }
+        components: { Table, Applications }
     })
     export default class ScheduleBlock extends Vue {
         @Prop({ default: '' }) filter;
@@ -84,104 +81,27 @@
 <style lang="scss" scoped>
 @import '../../assets/styles/utils';
 
-tbody td {
-    &:last-of-type {
+.Table {
+    td:last-of-type {
         text-align: right;
-    }
-}
-
-@include breakpoint(sm) {
-    thead {
-        display: none;
-    }
-
-    tbody tr {
-        display: block;
-        padding: $xs-space $grid-gutter-part;
-        text-align: center;
-        cursor: default;
-
-        &:hover {
-            background-color: transparent;
-        }
-
-        &:not(:last-of-type) {
-            border-bottom: 1px solid $divider-color;
-        }
-    }
-
-    tbody td {
-        display: block;
-        padding: 0;
-        border: none;
-
-        &:nth-of-type(1),
-        &:nth-of-type(2) {
-            display: inline-block;
-            vertical-align: middle;
-        }
-
-        &:last-of-type {
-            margin-top: $xs-space;
-            text-align: center;
-        }
-    }
-
-    .btn {
-        margin-top: $xxs-space;
     }
 }
 
 .dow {
     text-transform: uppercase;
     font-size: $font-size-sm;
-
-    @include breakpoint(sm) {
-        &:after {
-            content: ",";
-            margin-right: $xxs-space;
-        }
-    }
-}
-
-.time {
-    @include breakpoint(sm) {
-        &:before {
-            content: "@";
-            margin: 0 $xxs-space;
-        }
-    }
-}
-
-.duration {
-    &:after {
-        content: " min";
-        font-size: $font-size-xs;
-    }
-}
-
-td div,
-td h4:not(.duration) {
-    @include breakpoint(sm) {
-        display: inline-block;
-        vertical-align: middle;
-        font-size: $font-size-base !important;
-        text-transform: uppercase;
-    }
 }
 
 .show {
     text-transform: uppercase;
-
-    @include breakpoint(sm) {
-        &:after {
-            content: "-";
-            margin: 0 $xxs-space;
-        }
-    }
 }
 
-tbody tr.tournament .show {
+.duration:after {
+    content: " min";
+    font-size: $font-size-xs;
+}
+
+.tournament .show {
     color: $bonus-color;
 
     &:before {
