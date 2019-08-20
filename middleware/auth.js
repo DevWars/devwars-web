e5eb2f01ee61e51fc827dd78f862a23fc54c4a2e
SYNTAG <accounts@blucitrus.com>
2019-08-20 05:17:40 -0700

    Update auth to token

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,18 @@ import { isAuthorized, sameRole } from '../utils/auth';
 
 export default ({ store, route, redirect, error }) => {
     route.meta.map((meta) => {
-        if (meta.auth) {
+        if (meta.token) {
             if (!store.state.user.user) return redirect('/login');
 
             const role = store.state.user.user.role || 'PENDING';
 
-            if (typeof meta.auth !== 'string')
+            if (typeof meta.token !== 'string')
                 return error({
                     statusCode: 500,
                     message: 'meta auth should be a string',
                 });
 
-            const ok = isAuthorized(role, meta.auth);
+            const ok = isAuthorized(role, meta.token);
 
             if (!ok && meta.redirectIfNot) return redirect(meta.redirectIfNot);
             if (!ok) {
@@ -27,7 +27,7 @@ export default ({ store, route, redirect, error }) => {
             // Temporarily remove PENDING check
             // if (role === 'PENDING' && route.name !== 'pending') return redirect('/pending');
 
-            if (meta.limit && !sameRole(role, meta.auth)) {
+            if (meta.limit && !sameRole(role, meta.token)) {
                 if (meta.redirectIfNot) return redirect(meta.redirectIfNot);
                 return redirect('/');
             }
