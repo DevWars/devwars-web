94d420ff747edb3fa6a666c439f0a1a8e2509f58
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-05-02 17:56:29 +0100

    feat: games and schedules now use paging implementation
    Anything that was using /active or related status endpoints now uses the paging implementation.

--- pages/mod/schedules.vue
+++ pages/mod/schedules.vue
@@ -6,7 +6,10 @@
             </ButtonIcon>
         </PanelHeader>
 
-        <ListingFilters />
+        <ListingFilters
+            :options="scheduleFilterOptions"
+            @change="onScheduleFilterChange"
+        />
 
         <Table>
             <tr slot="head">
@@ -17,7 +20,7 @@
                 <th>&nbsp;</th>
             </tr>
 
-            <tr v-for="schedule in schedules" :key="schedule.id">
+            <tr v-for="schedule in schedules.data" :key="schedule.id">
                 <td>{{ schedule.startTime | moment('MM/DD/YYYY') }}</td>
                 <td>
                     <span
@@ -41,13 +44,24 @@
                 </td>
             </tr>
         </Table>
+
+        <Pagination
+            :page="page"
+            :per-page="10"
+            :can-next="canPageNextSchedule"
+            :can-previous="canPagePreviousSchedule"
+            @next="next"
+            @previous="previous"
+        />
     </div>
 </template>
 
 <script>
+import { isNil } from 'lodash';
 import { names } from '../../utils/auth';
 import CreateScheduleModal from '../../components/modal/CreateScheduleModal.vue';
 import PanelHeader from '@/components/mod/PanelHeader';
+import Pagination from '@/components/Pagination';
 import ListingFilters from '@/components/mod/ListingFilters';
 import Table from '@/components/Table';
 
@@ -60,15 +74,54 @@ export default {
         auth: names.MODERATOR,
     },
 
-    components: { PanelHeader, ListingFilters, Table },
+    components: { PanelHeader, ListingFilters, Table, Pagination },
 
-    async fetch({ store }) {
-        await store.dispatch('game/schedules');
+    async asyncData({ query, error, $axios }) {
+        try {
+            const { data } = await $axios.get('/schedules?first=10');
+            return { schedules: data, page: 0 };
+        } catch (e) {
+            error({
+                statusCode: e.response.status,
+                description: e.response.data.error,
+                message: e.response.statusText,
+            });
+        }
+    },
+
+    data() {
+        return {
+            scheduleFilter: 'all',
+
+            scheduleFilterOptions: [
+                { name: 'Show All', value: 'all' },
+                { name: 'Scheduled', value: 'scheduled' },
+                { name: 'Active', value: 'active' },
+                { name: 'Ended', value: 'ended' },
+            ],
+        };
     },
 
     computed: {
-        schedules() {
-            return this.$store.state.game.schedules;
+        /**
+         * Returns true if we can page forward in the schedule list, this ensures
+         * we have the next page link and the next page will not be empty.
+         */
+        canPageNextSchedule() {
+            if (isNil(this.schedules?.pagination?.next)) return false;
+            return (
+                this.schedules?.pagination?.next != null &&
+                this.schedules?.data?.length >= 10
+            );
+        },
+
+        /**
+         * Returns true if we can page backward, ensuring that we are not the
+         * first page.
+         */
+        canPagePreviousSchedule() {
+            if (isNil(this.schedules?.pagination?.previous)) return false;
+            return this.schedules?.pagination?.previous != null;
         },
     },
 
@@ -76,6 +129,56 @@ export default {
         nameFromStatus,
         async createSchedule() {
             await this.$open(CreateScheduleModal, {});
+        },
+
+        async onScheduleFilterChange(e) {
+            try {
+                this.scheduleFilter = e;
+                let url = '/schedules?first=10';
+
+                if (this.scheduleFilter !== 'all')
+                    url += `&status=${this.scheduleFilter}`;
+
+                this.schedules = (await this.$axios.get(url)).data;
+
+                this.page = 0;
+            } catch (error) {
+                this.$store.dispatch('toast/error', e.response.data);
+            }
+        },
+
+        /**
+         * Execute a page backward.
+         */
+        async previous() {
+            this.page -= 1;
+            const { pagination } = this.schedules;
+            const before = pagination.previous;
+
+            let url = `schedules?first=10&before=${before}`;
+            if (this.scheduleFilter !== 'all')
+                url += `&status=${this.scheduleFilter}`;
+
+            const { data } = await this.$axios.get(url);
+
+            this.schedules = data;
+        },
+
+        /**
+         * Execute a page forward.
+         */
+        async next() {
+            this.page += 1;
+            const { pagination } = this.schedules;
+            const after = pagination.next;
+
+            let url = `schedules?first=10&after=${after}`;
+            if (this.scheduleFilter !== 'all')
+                url += `&status=${this.scheduleFilter}`;
+
+            const { data } = await this.$axios.get(url);
+
+            this.schedules = data;
         },
     },
 };
